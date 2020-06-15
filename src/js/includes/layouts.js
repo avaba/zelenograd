5e367114043998d0f6bdd6f6856063088e37da75
avaba <42552832+avaba@users.noreply.github.com>
2020-06-15 07:30:56 +0300

    +

--- src/js/includes/layouts.js
+++ src/js/includes/layouts.js
@@ -1,4 +1,5 @@
 $(function(){ 
+
 // Слайдер Планировки
 
 $('.layouts-slide').slick({
@@ -7,5 +8,25 @@ $('.layouts-slide').slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
+  responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 2,
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+    }
+  },
+  ]
 });
 });

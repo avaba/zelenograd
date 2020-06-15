33f7f1be0d09446ef4bc2c91db45be95b8466c95
avaba <42552832+avaba@users.noreply.github.com>
2020-06-15 12:39:07 +0300

    mobile

--- src/js/includes/infrastructure.js
+++ src/js/includes/infrastructure.js
@@ -7,5 +7,25 @@ $('.infrastructure-slider').slick({
 	speed: 300,
 	slidesToShow: 4,
 	slidesToScroll: 1,
+	responsive: [
+	{
+		breakpoint: 1200,
+		settings: {
+			slidesToShow: 3,
+		}
+	},
+	{
+		breakpoint: 768,
+		settings: {
+			slidesToShow: 2,
+		}
+	},
+	{
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 1,
+		}
+	},
+	]
 });
 });
